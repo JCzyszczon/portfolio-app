a9b6f9314c74052875f7dd8352756a098f764b22
JCzyszczon <jakub.czyszczon1@gmail.com>
2024-07-12 00:35:51 +0200

    fix

--- app/components/softSkills.jsx
+++ app/components/softSkills.jsx
@@ -6,7 +6,7 @@ function SoftSkills() {
     <motion.article
       initial={{ height: 0, opacity: 0 }}
       animate={{ height: "100%", opacity: 1 }}
-      exit={{ height: 0, opacity: 0 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
       className='absolute left-0 bottom-0 w-full h-full navbar rounded-xl p-10'
     >
