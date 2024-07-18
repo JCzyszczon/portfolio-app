3212f2b51705cdd2e25dfd92f32d71dc79c78199
JCzyszczon <jakub.czyszczon1@gmail.com>
2024-07-18 23:58:49 +0200

    update

--- app/layout.js
+++ app/layout.js
@@ -22,17 +22,11 @@ export default function RootLayout({ children }) {
         />
         <link
           rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='./favicon-32x32.png'
+          href='/favicon-32x32?<generated>'
+          type='image/<generated>'
+          sizes='<generated>'
         />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='./favicon-16x16.png'
-        />
-        <link rel='manifest' href='./site.webmanifest' />
+        <link rel='icon' href='/favicon.ico' sizes='any' />
         <link rel='mask-icon' href='./safari-pinned-tab.svg' color='#d55bd1' />
         <meta name='msapplication-TileColor' content='#2b5797' />
       </head>
